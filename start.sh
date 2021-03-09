eb5e1adeecc168fc33e8ab7b895a653d304b70b1
mungai-joel <mungaijoel123@gmail.com>
2021-03-09 09:47:02 +0300

    migrations

---
+++ start.sh
@@ -0,0 +1,8 @@
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moonguy:notmoonguy@localhost/blog'
+export SECRET_KEY=1234
+export MAIL_USERNAME=fugimungai@gmail.com
+export MAIL_PASSWORD=1234
+
+
+
+python3.8 manage.py server
